8743a317eea49c72ea746400ac5a61c656154aff
abbattar <abbattar@mail.ru>
2022-11-23 08:57:43 +0300

    match-case

--- quart_2/seminar_8/menu.py
+++ quart_2/seminar_8/menu.py
@@ -21,24 +21,33 @@ def menu():
         '\t7. Очистить БД\n'
         '\t8. Закрыть программу\n'
         f'{"*" * 50}\n'
-        '\tВведите номер действия и нажмите Enter: '))
-
+        '\tВведите номер действия и нажмите Enter: ', encoding='utf-8'))
+    # match choise:
+        # case '1':
     if choice == '1':
         conn.create()
+        # case '2':
     elif choice == '2':
         conn.generation_users()
+        # case '3':
     elif choice == '3':
         conn.view_users()
+        # case '4':
     elif choice == '4':
         conn.add_user()
+        # case '5':
     elif choice == '5':
         conn.edit_user()
+        # case '6':
     elif choice == '6':
         conn.delete_user()
+        # case '7':
     elif choice == '7':
         conn.delete_all()
+        # case '8':
     elif choice == '8':
         lg.logging.info('Программа закрыта. Всего ВАМ ДО! БРО! ГО! )))')
         sys.exit('Программа закрыта. Всего ВАМ ДО! БРО! ГО! )))')
+        # case _:
     else:
         print('Что-то пошло не так. Повторите ввод!')
